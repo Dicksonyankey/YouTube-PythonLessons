a239e5e00179e9e3424a258efec2cabb8fe624b0
dicksonyankey <yankeydickson1@gmail.com>
2023-01-05 00:32:21 +0000

    Added some content to the file

--- Lists /list_and_methods.py
+++ Lists /list_and_methods.py
@@ -1,5 +1,5 @@
 # LIST DEFINITIONS
-# A list is a data structure that holds an ordered collection of items i.e. we can store a sequence of items in a list. In Python, lists are created by placing all items within square brackets [] separated by comma.
+# A list is a data structure that holds an ordered collection of items i.e. we can store a sequence of items in a list. In Python, lists are created by placing all items within square brackets [] separated by comma.Moreover, the lists, due to their mutable nature, can be altered after their creation.We will talk more about list mutability.
 
 
 # CREATING A LIST IN PYTHON
@@ -41,8 +41,35 @@ print(list_of_jobs[0]) # this will display the first item in list.ie. "Data Scie
 print(list_of_jobs[1]) # this will display the first item in list.ie. "Web Developer"
 print(list_of_jobs[2]) # this will display the first item in list.ie. "Hacker"
 
+# LISTS MUTABILITY IN PYTHON
+
+# List mutability in Python means that one can change an item present in a list by accessing it directly as part of the assignment statement. Furthermore, one can update one of the list items by using the indexing operator on the left side of an assignment.
+
+# Creating a list of cars
+list_of_cars = ["Toyoto","Mercedes","Ford","Jeep","Audi","BMW"]
+
+# Altering changes after creation : changing Mercedes to Kia
+list_of_cars[1] = "Kia"
+
+# Altering changes after creation : changing Toyota to Volvo
+list_of_cars[0] = "Volvo"
+
+# dispplay the list of cars to see the changes
+print(list_of_cars)
 
 
+# LIST SLICING IN PYTHON 
+# We can equally slice a list alos using the square bracket [],but the bracket will have a accept some kind of parametersand a colon. that's 1. start 2. stop and 3. step that's [start:stop:step] . i want to mention that the stop is with -1
+
+# slicing the sub element from the list of cars : i want to select from index 0-3
+new_cars = list_of_cars[0:3]
+print(new_cars)
+
+
+# slicing the last 3 cars from the list of cars.
+
+last_three = list_of_cars[3:]
+print(last_three)
 
 
 
